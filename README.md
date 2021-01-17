674fb2da985b0ccc0c72aad2459c11135b1656b9
hasithsen <it17142038@my.sliit.lk>
2021-01-18 00:22:44 +0530

    Add background image preload

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # bunny-jump
-Simple web game with Phaser 3
+Simple web game with Phaser 3 based on [this book](https://ourcade.co/books/infinite-jumper-phaser3/).
---
+++ src/main.js
@@ -0,0 +1,10 @@
+import { Game } from 'phaser';
+import Phaser from './lib/phaser.js';
+import Game from './scenes/Game.js';
+
+export default new Phaser.Game({
+  type: Phaser.AUTO,
+  width: 480,
+  height: 640,
+  scene: Game
+});
